1c3647d649c7797f3c20724c4633477d7a86924d
bpoumeau <delafforest.brieuc@gmail.com>
2023-02-17 17:16:55 +0100

    init pattern call in main

--- src/main.c
+++ src/main.c
@@ -6,18 +6,22 @@
 /*   By: bpoumeau <bpoumeau@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 16:58:21 by bpoumeau          #+#    #+#             */
-/*   Updated: 2023/02/17 17:04:04 by bpoumeau         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:16:42 by bpoumeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#include "../philo.h"
 
 static	int	_main_ending_process(char *msg);
 
 int	main(int ac, char *av[])
 {
+	t_philo_id	pattern;
+
 	if (ac < 4 || ac > 5)
-		return (_main_ending_process("invalid argument number.\n"));
+		return (_main_ending_process("Invalid argument number.\n"));
+	if (init_pattern(&pattern, av[2]) != SUCCESS)
+		return (_main_ending_process("Invalid argument.\n"))
 	return (0);
 }
 
