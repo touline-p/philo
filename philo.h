9d089eb45012606e2853cb3fd703a5d044d2c86f
bpoumeau <delafforest.brieuc@gmail.com>
2023-02-17 17:10:31 +0100

    adding t_philo_id struct

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: bpoumeau <bpoumeau@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 16:59:21 by bpoumeau          #+#    #+#             */
-/*   Updated: 2023/02/17 17:04:43 by bpoumeau         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:09:57 by bpoumeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 # define PHILO_HILO_H
 
 # include "libft.h"
+
+typedef struct s_philo_id
+{
+	int	ttd;
+	int	tte;
+	int	tts;
+	int	philo_nb;
+}	t_philo_id;
+
 
 #endif
