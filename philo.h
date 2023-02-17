48ffb4ff1d47172473f271050a7159e4199fe41c
bpoumeau <delafforest.brieuc@gmail.com>
2023-02-17 17:18:17 +0100

    init pattern proto

--- philo.h
+++ philo.h
@@ -6,14 +6,14 @@
 /*   By: bpoumeau <bpoumeau@student.42lyon.f>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 16:59:21 by bpoumeau          #+#    #+#             */
-/*   Updated: 2023/02/17 17:09:57 by bpoumeau         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:15:33 by bpoumeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_HILO_H
 # define PHILO_HILO_H
 
-# include "libft.h"
+# include "libft/libft.h"
 
 typedef struct s_philo_id
 {
@@ -23,5 +23,14 @@ typedef struct s_philo_id
 	int	philo_nb;
 }	t_philo_id;
 
+/*** INIT ***/
+/**
+ * @param pattern_ptr
+ * @param arguments
+ * @return SUCCESS if args are valid
+ * @return FAILURE if not
+ */
+
+t_ert	init_pattern(t_philo_id *pattern_ptr, char **arguments);
 
 #endif
